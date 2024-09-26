3d071362071491504a363c3766321808d95eea30
Caden Wilcox <wilcox4357@gmail.com>
2024-09-26 15:58:55 -0700

    wk 7 algos

---
+++ week4/day1/OH/Algos.js
@@ -0,0 +1,12 @@
+// write a function takes in an object and returns an array of all the values in the object that are numbers
+
+// solution 1
+
+// https://www.hackerrank.com/challenges/cats-and-a-mouse/problem?isFullScreen=true
+
+// solution to cats and a mouse
+
+
+// https://www.hackerrank.com/challenges/migratory-birds/problem?isFullScreen=true
+
+// solution to migratory birds
